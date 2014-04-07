b79e43ae6eaf6a2e8a7bad08277e9e72f50ab437
giorgio <giorgio@localhost.localdomain>
2014-04-07 15:41:07 +0200

    First commit

---
+++ asd2014-01/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: giorgio
+ *
+ * Created on 7 aprile 2014, 15.40
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+    cout << "Funziona yaaay!" << endl;
+    return 0;
+}
+
