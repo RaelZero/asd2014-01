cb5d1402337794885567a0044a6f194d6429718a
giorgio <giorgio@localhost.localdomain>
2014-04-08 09:30:52 +0200

    scheletro di base

--- asd2014-01/main.cpp
+++ asd2014-01/main.cpp
@@ -5,17 +5,61 @@
  * Created on 7 aprile 2014, 15.40
  */
 
-#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
 
 using namespace std;
+
+
+struct node
+{
+    vector<int> adiacenti;
+    int archiEntranti = 0;
+    bool visitato;
+};
+
 
 /*
  * 
  */
+
+int n = 0;              // numero di nodi
+int m = 0;              // numero di archi
+vector<node> grafo;     // grafo di input
+
+
+vector<node> leggiGrafo(string fileName)
+{
+    vector<node> grafo;
+    node nodo;
+    
+    ifstream f("/home/giorgio/NetBeansProjects/asd2014-01/asd2014-01/asd2014-01/DatasetP1/input1.txt");
+    f >> n >> m;
+    graph.resize(n);
+    
+    for (int i = 0; i < m; i++)
+    {
+        int from, to;
+        f >> from >> to;
+        grafo[from].adiacenti.push_back(to);
+        grafo[to].archiEntranti++;
+    }
+    
+    f.close();
+    return grafo;
+}
+
+void topologicOrder()
+{
+    
+}
+
 int main(int argc, char** argv)
 {
-    cout << "Funziona yaaay!" << endl;
+    grafo = leggiGrafo();
+    
     return 0;
 }
 
