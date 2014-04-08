1f7241f33fd840a58f52ebe2ab185ee09e9ac0e8
RaelZero <gaz.gong@gmail.com>
2014-04-08 13:29:33 +0200

    Commenti su tutto il codice

--- asd2014-01/main.cpp
+++ asd2014-01/main.cpp
@@ -33,12 +33,15 @@ stack<int> consiglieri;
 int numeroConsiglieri = 0;
     
 
+// Importa il grafo
 void leggiGrafo()
 {
-    ifstream f("/home/giorgio/NetBeansProjects/asd2014-01/asd2014-01/asd2014-01/DatasetP1/input1.txt");
+    // Importa il numero di nodi e archi
+    ifstream f("input.txt");
     f >> n >> m;
     grafo.resize(n);
     
+    // Cicla sul numero di archi, importa il grafo
     for (int i = 0; i < m; i++)
     {
         int from, to;
@@ -50,13 +53,17 @@ void leggiGrafo()
     f.close();
 }
 
+// 
 void ordineTopologico()
 {
-   ofstream f("/home/giorgio/NetBeansProjects/asd2014-01/asd2014-01/asd2014-01/DatasetP1/output.txt");
-   vector<int> outputCons;
+    // Inizializza il file di output e il vettore dei consiglieri 
+    ofstream f("output.txt");
+    vector<int> outputCons;
     
+    // Cicla su tutti i nodi
     for (int i = 0; i < n; i++)
     {
+        // Se ci sono 0 archi entranti, il nodo è un consigliere
         if (grafo[i].archiEntranti == 0)
         {
             consiglieri.push(i);
@@ -65,49 +72,69 @@ void ordineTopologico()
     }
     
     numeroConsiglieri = consiglieri.size();
-    
+   
+    // Pila dei nodi da esplorare, come indici 
     stack<int> esplora;
-    int i;
     
-    //cout << "numero consiglieri: " << numeroConsiglieri << endl;
+    //cout << "numero consiglieri: " << numeroConsiglieri << endl; //DEBUG
+    // Scrive sul file di output il numero dei consiglieri
     f << numeroConsiglieri << endl;
     
+    // Cicla per scrivere sul file di output tutti i consiglieri
     for (int g = 0; g < outputCons.size(); g++)
     {
         f << outputCons[g] << " ";
     }
     f << endl;
-    
+   
+    // Cicla sui consiglieri 
     while (!consiglieri.empty())
     {
-        i = consiglieri.top();
+        // Setta i all'indice del consigliere
+        int i = consiglieri.top();
         
-        //cout << "consigliere: " << i << endl;
+        // Estrae il consigliere i-esimo e lo flagga come visitato
+        //cout << "consigliere: " << i << endl; //DEBUG
         consiglieri.pop();
         grafo[i].visitato = true;
-        
+       
+        // Cicla su tutti gli adiacenti a quel consigliere 
         for (int j = 0; j < grafo[i].adiacenti.size(); j++)
         {
-            //cout << "i -- grafo[i], adiacenti[j]: " << i << " -- " << grafo[i].adiacenti[j] << endl;
+            //cout << "i -- grafo[i], adiacenti[j]: " << i << " -- " << grafo[i].adiacenti[j] << endl; //DEBUG
+
+            // Se il j-esimo vicino non è ancora stato visitato, lo mette in pila
             if (!grafo[j].visitato)
-                {
+            {
+                // Scrive la riga del tipo "il generale i-esimo comanda il vicino j-esimo"
                 f << i << " " << grafo[i].adiacenti[j] << endl;
 
+                // Impila il vicino j-esimo per l'esplorazione
                 esplora.push(grafo[i].adiacenti[j]);
+    
+                // Cicla finché la pila di esplorazione non è vuota
                 while(!esplora.empty())
                 {
+                    // Estrae il primo elemento dalla pila
                     int k = esplora.top();
                     esplora.pop();
+                
+                    // Se il nodo estratto non è già stato visitato
                     if(!grafo[k].visitato)
                     {
+                        // Flagga come visitato
                         grafo[k].visitato = true;
+
+                        // Cicla sugli adiacenti a quel nodo
                         for (int w = 0; w < grafo[k].adiacenti.size(); w++)
                         {
-                            // cout << "k -- grafo[k], adiacenti[w]: " << k << " -- " << grafo[k].adiacenti[w] << endl;
+                            // cout << "k -- grafo[k], adiacenti[w]: " << k << " -- " << grafo[k].adiacenti[w] << endl; //DEBUG
+                            // Se l'adiacente non è già stato visitato
                             if(!grafo[w].visitato)
                             {
-                                f << k << " " << grafo[k].adiacenti[w] << endl;
+                                // Impila per l'esplorazione e scrive in output "Il vicino k comanda w"
                                 esplora.push(grafo[k].adiacenti[w]);
+                                f << k << " " << grafo[k].adiacenti[w] << endl;
                             }
                         }
                     }
