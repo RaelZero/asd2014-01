c9fc6e5ae9321a16310d7ff640ebdf81df3d1226
RaelZero <gaz.gong@gmail.com>
2014-04-09 13:23:13 +0200

    Intanto compila

--- generali.cpp
+++ generali.cpp
@@ -31,129 +31,15 @@ stack<int> consiglieri;     // Vettore di consiglieri
 int numeroConsiglieri = 0;  // Numero di consiglieri
 ofstream f("output.txt");
     
+// Firme dei metodi
+void leggiGrafo();
+void cfc();
+void dfsStack(stack<int> &s, int nodo, vector<bool> &v);
+void cc(vector<int> &v);
+void ccdfs(int componente, int i);
+void ordineTopologico(int nodo);
+void tagliaCicli();
 
-// Importa il grafo
-void leggiGrafo()
-{
-    // Importa il numero di nodi e archi
-    ifstream f("input.txt");
-    f >> n >> m;
-    grafo.resize(n);
-    trasposto.resize(n);
-    
-    //cout << n << " " << m; //DEBUG
-    // Cicla sul numero di archi importa il grafo, tracciando il numero di archi entranti per nodo
-    for (int i = 0; i < m; i++)
-    {
-        int from, to;
-        f >> from >> to;
-        
-        // Importa nel grafo
-        grafo[from].adiacenti.push_back(to);
-        grafo[to].archiEntranti++;
-        
-        // Importa nel grafo trasposto
-        trasposto[to].adiacenti.push_back(from);
-        trasposto[from].archiEntranti++;
-    }
-    
-    f.close();
-}
-
-// Flagga ogni nodo con il numero della sua componente connessa
-void cfc()
-{
-    stack<int> s;
-
-    // Crea e inizializza l'array di appoggio per la visita delle CFC
-    vector<bool> v;
-    v.resize(n);
-    for(int i = 0; i < v.size(); i++)
-        v[i] = false;
-    
-    for(int i = 0; i < graph.size(); i++)
-    {
-        dfsStack(&s, i, &v);
-    }
-
-    vector<int> ordine;
-    ordine.resize(n);
-
-    for(int i = 0; i < ordine.size(); i++)
-    {
-        ordine[i] = s.top();
-        s.pop();
-    }
-
-    cc(vector<int> &ordine);
-}
-
-// Funzione ausiliaria per caricare la pila di cfc()
-void dfsStack(stack<int> &s, int nodo, vector<bool> &v)
-{
-    for (int i = 0; i < ((grafo[nodo]).adiacenti[i]).size(); i++)
-    {
-        if(!v[((grafo[nodo]).adiacenti[i])])
-        {
-            dfsStack(&s, grafo[nodo].adiacenti[i], &v);
-        }
-    }
-    
-    s.push(nodo);
-}
-
-// Funzione per le componenti connesse
-void cc(vector<int> &v)
-{
-    int numComponente = 0;
-
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (trasposto[i].cfc == 0)
-        {
-            numComponente++;
-            ccdfs(numComponente, i);
-    }
-}
-
-// Funzione ausiliaria per la ricerca delle componenti connesse
-void ccdfs(int componente, int i)
-{
-    grafo[i] = componente;
-    trasposto[i] = componente;
-    
-    for (int j = 0; j < trasposto[i].adiacenti.size())
-    {
-        if (trasposto[i].adiacente[j].cfc == 0)
-        {
-            ccdfs(componente, trasposto[i].adiacente[j]);
-        }
-    }
-}
-
-
-// Soluzione da 30 punti
-void ordineTopologico(int nodo)
-{
-    if (!grafo[nodo].visitato)
-    {
-        grafo[nodo].visitato = true;
-        for (int i = 0; i < grafo[nodo].adiacenti.size(); i++)
-        {
-            //cout << "nodo: " << nodo << " i: " << grafo[nodo].adiacenti[i] << "\n"; //DEBUG
-            if(!grafo[grafo[nodo].adiacenti[i]].visitato)
-            {
-                f << nodo << " " << grafo[nodo].adiacenti[i] << endl;
-                ordineTopologico(grafo[nodo].adiacenti[i]);
-            }
-        }
-    }
-}
-
-void tagliaCicli(){
-
-
-}
 
 int main(int argc, char** argv)
 {
@@ -197,5 +83,130 @@ int main(int argc, char** argv)
     }
     
     return 0;
+}
+
+// Importa il grafo
+void leggiGrafo()
+{
+    // Importa il numero di nodi e archi
+    ifstream f("input.txt");
+    f >> n >> m;
+    grafo.resize(n);
+    trasposto.resize(n);
+    
+    //cout << n << " " << m; //DEBUG
+    // Cicla sul numero di archi importa il grafo, tracciando il numero di archi entranti per nodo
+    for (int i = 0; i < m; i++)
+    {
+        int from, to;
+        f >> from >> to;
+        
+        // Importa nel grafo
+        grafo[from].adiacenti.push_back(to);
+        grafo[to].archiEntranti++;
+        
+        // Importa nel grafo trasposto
+        trasposto[to].adiacenti.push_back(from);
+        trasposto[from].archiEntranti++;
+    }
+    
+    f.close();
+}
+
+// Flagga ogni nodo con il numero della sua componente connessa
+void cfc()
+{
+    stack<int> s;
+
+    // Crea e inizializza l'array di appoggio per la visita delle CFC
+    vector<bool> v;
+    v.resize(n);
+    for(int i = 0; i < v.size(); i++)
+        v[i] = false;
+    
+    for(int i = 0; i < grafo.size(); i++)
+    {
+        dfsStack(s, i, v);
+    }
+
+    vector<int> ordine;
+    ordine.resize(n);
+
+    for(int i = 0; i < ordine.size(); i++)
+    {
+        ordine[i] = s.top();
+        s.pop();
+    }
+
+    cc(ordine);
+}
+
+// Funzione ausiliaria per caricare la pila di cfc()
+void dfsStack(stack<int> &s, int nodo, vector<bool> &v)
+{
+    for (int i = 0; i < ((grafo[nodo]).adiacenti).size(); i++)
+    {
+        if(!v[((grafo[nodo]).adiacenti[i])])
+        {
+            dfsStack(s, grafo[nodo].adiacenti[i], v);
+        }
+    }
+    
+    s.push(nodo);
+}
+
+// Funzione per le componenti connesse
+void cc(vector<int> &v)
+{
+    int numComponente = 0;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (trasposto[i].cfc == 0)
+        {
+            numComponente++;
+            ccdfs(numComponente, i);
+        }
+    }
+}
+
+// Funzione ausiliaria per la ricerca delle componenti connesse
+void ccdfs(int componente, int i)
+{
+    grafo[i].cfc = componente;
+    trasposto[i].cfc = componente;
+    
+    for (int j = 0; j < (trasposto[i].adiacenti).size(); j++)
+    {
+        if (trasposto[trasposto[i].adiacenti[j]].cfc == 0)
+        {
+            ccdfs(componente, trasposto[i].adiacenti[j]);
+        }
+    }
+}
+
+
+// Soluzione da 30 punti
+void ordineTopologico(int nodo)
+{
+    if (!grafo[nodo].visitato)
+    {
+        grafo[nodo].visitato = true;
+        for (int i = 0; i < grafo[nodo].adiacenti.size(); i++)
+        {
+            //cout << "nodo: " << nodo << " i: " << grafo[nodo].adiacenti[i] << "\n"; //DEBUG
+            if(!grafo[grafo[nodo].adiacenti[i]].visitato)
+            {
+                f << nodo << " " << grafo[nodo].adiacenti[i] << endl;
+                ordineTopologico(grafo[nodo].adiacenti[i]);
+            }
+        }
+    }
+}
+
+void tagliaCicli()
+{
+
+
 }
 
