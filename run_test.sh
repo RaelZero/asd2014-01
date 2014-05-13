4455378716eb004995c67bbce16cf8c24043e8a1
RaelZero <gaz.gong@gmail.com>
2014-05-13 18:15:46 +0200

    Aggiornato per il secondo progetto

--- run_test.sh
+++ run_test.sh
@@ -8,8 +8,8 @@ range=0..`echo $tests`
 
 for i in $(eval echo {$range})
 do
-        cp DatasetP1/input/input$i.txt input.txt
+        cp DatasetP2/input/input$i.txt input.txt
         echo -n "$i: "
-        (time ./generali) &> /tmp/res && cat /tmp/res | grep real 
-        ./DatasetP1/correttore DatasetP1/input/input$i.txt DatasetP1/helper/helper$i.txt output.txt
+        (time ./prescelto) &> /tmp/res && cat /tmp/res | grep real 
+        ./DatasetP2/correttore DatasetP2/input/input$i.txt DatasetP2/helper/helper$i.txt output.txt
 done
